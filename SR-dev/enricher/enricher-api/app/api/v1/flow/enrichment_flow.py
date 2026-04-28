d278592f9b12b2e761288b1f0765afa070461597
EmidioStani <emidiostani@gmail.com>
2026-04-28 18:47:29 +0200

    adding architecture and improve enricher

--- SR-dev/enricher/enricher-api/app/api/v1/flow/enrichment_flow.py
+++ SR-dev/enricher/enricher-api/app/api/v1/flow/enrichment_flow.py
@@ -112,13 +112,13 @@ def enrichment_flow(task: str = "all", job_id: str = None):
             #add_translations_to_graph.submit(source_endpoint, graph_uri, translate_future)
             
             # Step 0: delete
-            delete_source_descriptions_query = config["translate"]["delete_source_descriptions_query"]
-            delete_target_descriptions_query = config["translate"]["delete_target_descriptions_query"]
-            delete_descriptions_future = delete_descriptions.submit(source_endpoint, source_graph, target_graph, delete_source_descriptions_query, delete_target_descriptions_query, auth_dict)
+            #delete_source_descriptions_query = config["translate"]["delete_source_descriptions_query"]
+            #delete_target_descriptions_query = config["translate"]["delete_target_descriptions_query"]
+            #delete_descriptions_future = delete_descriptions.submit(source_endpoint, source_graph, target_graph, delete_source_descriptions_query, delete_target_descriptions_query, auth_dict)
             # Step 1: fetch
             languages = config["translate"]["languages"]
             fetch_descriptions_query = config["translate"]["fetch_descriptions_query"]
-            fetch_descriptions_future = fetch_descriptions_to_translate.submit(source_endpoint, source_graph, languages, fetch_descriptions_query, auth_dict, wait_for=[delete_descriptions_future])
+            fetch_descriptions_future = fetch_descriptions_to_translate.submit(source_endpoint, source_graph, languages, fetch_descriptions_query, auth_dict)
             # Step 2: Batch (as a Prefect task)
             batch_size = config["translate"]["batch_size"]
             batches_future = make_batches.submit(fetch_descriptions_future, batch_size)  # only one dependency here
